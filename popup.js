c0aecce97d436e59812ea4f18fcca2e658730a94
jake-waratahs <jakebloom744@gmail.com>
2017-11-28 21:45:18 -0800

    Javascript

--- popup.js
+++ popup.js
@@ -1,6 +1,32 @@
-function toggle(key){}
-
-var boxes = document.getElementsByTagName("input");
-for (var i = 0; i < boxes.length; i++) {
-    boxes[i].checked = true;
+function toggle(key){
+    chrome.storage.sync.get(key, function(items) {
+        var newSettings = {};
+        if (items[key]) {
+            newSettings[key] = false;
+        } else {
+            newSettings[key] = true;
+        }
+        chrome.storage.sync.set(newSettings);    
+    });
 }
+
+
+chrome.storage.sync.get(null, function(items){
+    var newSettings = {};
+
+    Object.keys(items).map(function(name) {
+        var box = document.getElementById(name);
+        box.checked = items[name];
+    });
+
+    var boxes = document.getElementsByTagName('input');
+
+    for (var i = 0; i < boxes.length; i++) {
+        if (items[boxes[i].id] == null) {
+            newSettings[boxes[i].id] = true;
+            boxes[i].checked = true;
+        }
+    }
+
+    chrome.storage.sync.set(newSettings);
+});
